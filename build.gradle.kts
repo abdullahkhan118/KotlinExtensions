a7696726fa7d303ee8247502d5ea3bddcf7c51fc
abdullahkhan <abdullahkhan1571998@gmail.com>
2023-12-16 15:29:54 +0500

    Release-v1.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,24 @@
+import org.apache.commons.logging.LogFactory.release
+
+
 plugins {
     kotlin("jvm") version "1.9.21"
     id("maven-publish")
 }
 
+apply {
+    plugin("maven-publish")
+}
+
 group = "com.github.abdullahkhan118"
-version = "1.0.0"
+version = "1.0.1"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.21")
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
 }
@@ -20,4 +28,17 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(21)
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["java"])
+                groupId = "com.github.abdullahkhan118"
+                artifactId = "kotlin.extensions"
+                version = "1.0.1"
+            }
+        }
+    }
 }
